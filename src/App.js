4fac97aeac9c9c29685aaa22adae327e7b90d149
Mukund <mukundkumar9999@gmail.com>
2024-07-29 15:03:24 +0530

    All Routes created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, Redirect } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import LoginRoute from "./components/LoginRoute";
+import AllNotes from "./components/AllNotes";
+import RegisterRoute from "./components/RegisterRoute";
+import CreateNote from "./components/CreateNote";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginRoute} />
+    <Route exact path="/register" component={RegisterRoute} />
+    <ProtectedRoute exact path="/" component={AllNotes} />
+    <ProtectedRoute exact path="/createNote" component={CreateNote} />
+    <Redirect to="/login" />
+  </Switch>
+);
 
 export default App;
